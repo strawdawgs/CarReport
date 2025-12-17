cbe620cb8513d7b1035bbb2c26dd42b2b5acd3a9
BrandonJaco <Brandon.jaco@tnsos.gov>
2025-12-17 14:53:52 -0600

    added the create service type form

--- DataModelLibrary/Models/ServiceType.cs
+++ DataModelLibrary/Models/ServiceType.cs
@@ -4,7 +4,8 @@ public class ServiceType
 {
     public int Id { get; set; }
     public string Type { get; set; } = string.Empty;
-    public string? RecommendedIntervalInMiles { get; set; }
-    public string? RecommendedIntervalInYears { get; set; }
+    public int? RecommendedIntervalInMilesMinimum { get; set; }
+    public int? RecommendedIntervalInMilesMaximum { get; set; }
+    public int? RecommendedIntervalInYears { get; set; }
     public ICollection<ServiceRecord> ServiceRecords { get; set; } = new List<ServiceRecord>();
 }
