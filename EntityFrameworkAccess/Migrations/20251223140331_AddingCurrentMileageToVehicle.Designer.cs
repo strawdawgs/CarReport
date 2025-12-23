81cf82a7ed758805c888a9570e4d594524697f49
BrandonJaco <Brandon.jaco@tnsos.gov>
2025-12-23 16:41:39 -0600

    first run at getting the quick grids for each car on the dashboard page

--- EntityFrameworkAccess/Migrations/20251223140331_AddingCurrentMileageToVehicle.Designer.cs
+++ EntityFrameworkAccess/Migrations/20251223140331_AddingCurrentMileageToVehicle.Designer.cs
@@ -4,6 +4,7 @@ using EntityFrameworkAccess.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EntityFrameworkAccess.Migrations
 {
     [DbContext(typeof(CarMaintenanceDbContext))]
-    partial class CarMaintenanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251223140331_AddingCurrentMileageToVehicle")]
+    partial class AddingCurrentMileageToVehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
